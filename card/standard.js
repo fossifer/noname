51668df1360e2bec5ebff440fdb26b86e4e6aecc
xizifu <2238983941@qq.com>
2025-03-10 12:14:28 +0800

    汉末南华老仙、眭固、手杀贾充fix；代传鬼神易的PR；旧杨芷【婉嫕】适配中流

--- card/standard.js
+++ card/standard.js
@@ -3420,6 +3420,9 @@ game.import("card", function () {
 					"step 0";
 					delete event.wuxieresult;
 					delete event.wuxieresult2;
+					delete event._result;
+					delete event.resultOL;
+					delete event._info_map;
 					//创建map存储各种信息，用于hasHiddenWuxie判断
 					var map = {};
 					event._info_map = map;
@@ -3601,7 +3604,7 @@ game.import("card", function () {
 						return current.hasWuxie(map);
 					});
 					event.list = list;
-					event.id = get.id();
+					if (!event.id) event.id = get.id();
 					map.id = event.id;
 					list.sortBySeat(_status.currentPhase);
 					"step 2";
@@ -3624,6 +3627,7 @@ game.import("card", function () {
 					"step 4";
 					var id = event.id;
 					var sendback = function (result, player) {
+						game.temp = result;
 						if (result && result.id == id && !event.wuxieresult && result.bool) {
 							event.wuxieresult = player;
 							event.wuxieresult2 = result;
@@ -3717,7 +3721,9 @@ game.import("card", function () {
 						}
 					}
 					"step 9";
-					if (event.wuxieresult) {
+					if (event?.wuxieresult2?.cancel) {
+						event.goto(0);
+					} else if (event.wuxieresult) {
 						var next = event.wuxieresult.useResult(event.wuxieresult2);
 						if (event.triggername != "phaseJudge") {
 							next.respondTo = [trigger.player, trigger.card];
