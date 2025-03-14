8c9b258d1c890cfec709a5b086e6eae5a92270e2
xizifu <2238983941@qq.com>
2025-03-14 12:18:53 +0800

    调整SP甄宓【惠济】、枣祗任峻【粮策】；修复【享乐】、【推心置腹】、曹髦【清正】祝融【蛮后】、曹媛【占欲】、

--- character/mobile/skill.js
+++ character/mobile/skill.js
@@ -5378,7 +5378,7 @@ const skills = {
 					event.dialog?.close();
 					event.control_ok?.close();
 					event.control_cancel?.close();
-					event._result = ai;
+					event._result = ai();
 					resolve(event._result);
 					game.resume();
 				};
